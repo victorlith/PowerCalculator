1c9c1fb96cfc1ebb6da88081b52820f1bd03824b
Victor Souza <victorsouzalith98@gmail.com>
2024-04-21 02:15:29 -0300

    Definido o tamanha da janela para Windows e corrigido o espaçamento entre os itens do CollectionView, no Windows e Android

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,16 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState) =>
+        new Window(new AppShell())
+        {
+            Width = 500,
+            Height = 700,
+            X = 650,
+            Y = 200
+        };
     }
 }
